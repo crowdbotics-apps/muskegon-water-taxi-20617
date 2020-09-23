8dd832121597ae501dc36443f7a9303565874326
Crowdbotics <team@crowdbotics.com>
2020-09-23 22:06:20 +0000

    Update models

--- backend/muskegon_water_taxi_20617/wsgi.py
+++ backend/muskegon_water_taxi_20617/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muskegon_water_taxi_20617.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "muskegon_water_taxi_20617.settings")
 
 application = get_wsgi_application()
